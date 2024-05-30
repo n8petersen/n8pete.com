7f4a78978205e36fb9338fb9cde2a952fe9eeee6
n8petersen <n8knows@gmail.com>
2024-05-29 22:50:59 -0600

    Increase rounding on borders

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.5rem',
+      'lg': '0.9rem',
+      'full': '9999px',
+      'large': '12px',
+    }
   },
   plugins: [],
 }
